f0c83cee01f8d7dcbea3f413e10767748d2a0e1d
marshall-ku <marshall@kakao.com>
2021-03-16 16:55:57 +0900

    Add lang to message

--- src/components/speaker.tsx
+++ src/components/speaker.tsx
@@ -1,5 +1,7 @@
 export default function speak(message: string) {
     const msg = new SpeechSynthesisUtterance();
+
     msg.text = message;
+    msg.lang = "en";
     window.speechSynthesis.speak(msg);
 }

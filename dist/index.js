066426e6285a091a6cf2370a2e889bc80370dfa3
marshall-ku <marshall@kakao.com>
2021-03-16 16:33:26 +0900

    Updates

--- dist/index.js
+++ dist/index.js
@@ -8938,6 +8938,13 @@ function SelectTable(props) {
   }));
 }
 
+// dist/components/speaker.js
+function speak(message) {
+  const msg = new SpeechSynthesisUtterance();
+  msg.text = message;
+  window.speechSynthesis.speak(msg);
+}
+
 // dist/components/Loader.js
 function Loader() {
   return /* @__PURE__ */ react.createElement("div", {
@@ -8984,9 +8991,77 @@ function Loader() {
   }))));
 }
 
+// dist/components/Icons.js
+function HomeIcon() {
+  return /* @__PURE__ */ react.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    height: "24px",
+    viewBox: "0 0 24 24",
+    width: "24px",
+    fill: "#FFFFFF"
+  }, /* @__PURE__ */ react.createElement("path", {
+    d: "M0 0h24v24H0V0z",
+    fill: "none"
+  }), /* @__PURE__ */ react.createElement("path", {
+    d: "M12 5.69l5 4.5V18h-2v-6H9v6H7v-7.81l5-4.5M12 3L2 12h3v8h6v-6h2v6h6v-8h3L12 3z"
+  }));
+}
+function ListIcon() {
+  return /* @__PURE__ */ react.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    height: "24px",
+    viewBox: "0 0 24 24",
+    width: "24px",
+    fill: "#FFFFFF"
+  }, /* @__PURE__ */ react.createElement("g", {
+    fill: "none"
+  }, /* @__PURE__ */ react.createElement("path", {
+    d: "M0 0h24v24H0V0z"
+  }), /* @__PURE__ */ react.createElement("path", {
+    d: "M0 0h24v24H0V0z",
+    opacity: ".87"
+  })), /* @__PURE__ */ react.createElement("path", {
+    d: "M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7zm-4 6h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z"
+  }));
+}
+function RenewIcon() {
+  return /* @__PURE__ */ react.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    height: "24px",
+    viewBox: "0 0 24 24",
+    width: "24px",
+    fill: "#FFFFFF"
+  }, /* @__PURE__ */ react.createElement("path", {
+    d: "M0 0h24v24H0V0z",
+    fill: "none"
+  }), /* @__PURE__ */ react.createElement("path", {
+    d: "M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z"
+  }));
+}
+function VolumeUpIcon() {
+  return /* @__PURE__ */ react.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    height: "24px",
+    viewBox: "0 0 24 24",
+    width: "24px",
+    fill: "#FFFFFF"
+  }, /* @__PURE__ */ react.createElement("path", {
+    d: "M0 0h24v24H0V0z",
+    fill: "none"
+  }), /* @__PURE__ */ react.createElement("path", {
+    d: "M3 9v6h4l5 5V4L7 9H3zm7-.17v6.34L7.83 13H5v-2h2.83L10 8.83zM16.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77 0-4.28-2.99-7.86-7-8.77z"
+  }));
+}
+
 // dist/router/Words.js
 function WordsList(props) {
   const {data, setData} = props;
+  const handleClick = (e2) => {
+    const target = e2.target;
+    console.log("hi");
+    console.log(target);
+    speak(target.dataset.message?.replace(/\(.*\)/, "") || "");
+  };
   return /* @__PURE__ */ react.createElement("div", {
     className: "words"
   }, /* @__PURE__ */ react.createElement("div", {
@@ -8998,7 +9073,11 @@ function WordsList(props) {
   }, "\uB73B"), /* @__PURE__ */ react.createElement("div", {
     className: "table__title"
   }, "\uC608\uBB38"), data.map((word) => {
-    return /* @__PURE__ */ react.createElement(react.Fragment, null, /* @__PURE__ */ react.createElement("div", null, word.word), /* @__PURE__ */ react.createElement("div", null, word.meaning.map((meaning) => {
+    return /* @__PURE__ */ react.createElement(react.Fragment, null, /* @__PURE__ */ react.createElement("div", null, word.word, /* @__PURE__ */ react.createElement("button", {
+      className: "",
+      "data-message": word.word,
+      onClick: handleClick
+    }, /* @__PURE__ */ react.createElement(VolumeUpIcon, null))), /* @__PURE__ */ react.createElement("div", null, word.meaning.map((meaning) => {
       return /* @__PURE__ */ react.createElement("li", null, meaning);
     })), /* @__PURE__ */ react.createElement("div", null, word.example || ""));
   })));
@@ -9041,54 +9120,6 @@ function Words() {
       className: "center-container"
     }, /* @__PURE__ */ react.createElement(Loader, null));
   }
-}
-
-// dist/components/Icons.js
-function HomeIcon() {
-  return /* @__PURE__ */ react.createElement("svg", {
-    xmlns: "http://www.w3.org/2000/svg",
-    height: "36px",
-    viewBox: "0 0 24 24",
-    width: "36px",
-    fill: "#FFFFFF"
-  }, /* @__PURE__ */ react.createElement("path", {
-    d: "M0 0h24v24H0V0z",
-    fill: "none"
-  }), /* @__PURE__ */ react.createElement("path", {
-    d: "M12 5.69l5 4.5V18h-2v-6H9v6H7v-7.81l5-4.5M12 3L2 12h3v8h6v-6h2v6h6v-8h3L12 3z"
-  }));
-}
-function ListIcon() {
-  return /* @__PURE__ */ react.createElement("svg", {
-    xmlns: "http://www.w3.org/2000/svg",
-    height: "36px",
-    viewBox: "0 0 24 24",
-    width: "36px",
-    fill: "#FFFFFF"
-  }, /* @__PURE__ */ react.createElement("g", {
-    fill: "none"
-  }, /* @__PURE__ */ react.createElement("path", {
-    d: "M0 0h24v24H0V0z"
-  }), /* @__PURE__ */ react.createElement("path", {
-    d: "M0 0h24v24H0V0z",
-    opacity: ".87"
-  })), /* @__PURE__ */ react.createElement("path", {
-    d: "M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7zm-4 6h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z"
-  }));
-}
-function RenewIcon() {
-  return /* @__PURE__ */ react.createElement("svg", {
-    xmlns: "http://www.w3.org/2000/svg",
-    height: "36px",
-    viewBox: "0 0 24 24",
-    width: "36px",
-    fill: "#FFFFFF"
-  }, /* @__PURE__ */ react.createElement("path", {
-    d: "M0 0h24v24H0V0z",
-    fill: "none"
-  }), /* @__PURE__ */ react.createElement("path", {
-    d: "M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z"
-  }));
 }
 
 // dist/router/Memorize.js
